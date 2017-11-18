7fc92c95c06d3febc88e3a70a35371acc0e72354
dawno <utkarshsinghcs@gmail.com>
2017-11-18 17:19:46 +0530

    Login and Registration Modules completed.

--- app/src/main/java/com/example/dellpc/uber/MainActivity.java
+++ app/src/main/java/com/example/dellpc/uber/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.dellpc.uber;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.SnackbarContentLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
+import Model.User;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class MainActivity extends AppCompatActivity {
-
-
+Button btnRegister,btnSignIn;
+FirebaseAuth auth;
+FirebaseDatabase db;
+DatabaseReference users;
+RelativeLayout rootLayout;
     @Override
     protected void attachBaseContext(Context newBase) {
         super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
@@ -20,5 +43,162 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder().setDefaultFontPath("fonts/Arkhip_font.ttf").setFontAttrId(R.attr.fontPath).build());
         setContentView(R.layout.activity_main);
+        auth= FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users= db.getReference("Users");
+        btnRegister= (Button)findViewById(R.id.btnRegister);
+        btnSignIn= (Button)findViewById(R.id.btnSignIn);
+        rootLayout= (RelativeLayout)findViewById(R.id.rootLayout);
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                showAlertDialog();
+            }
+        });
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showLoginDialog();
+            }
+        });
+
     }
-}
+
+    private void showLoginDialog() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("SIGN IN");
+        dialog.setMessage("Please use email to SIGN IN");
+        LayoutInflater inflator= LayoutInflater.from(this);
+        View login_layout= inflator.inflate(R.layout.layout_login,null);
+        final MaterialEditText edtEmail = login_layout.findViewById(R.id.edtEmail);
+
+        final MaterialEditText edtPassword = login_layout.findViewById(R.id.edtPassword);
+
+
+        dialog.setView(login_layout);
+
+        dialog.setPositiveButton("SIGN IN", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter email address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (edtPassword.getText().toString().length() < 6) {
+                    Snackbar.make(rootLayout, "Password is too short!!!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.signInWithEmailAndPassword(edtEmail.getText().toString(),edtPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(MainActivity.this,Welcome.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(rootLayout, "Failed!!"+e.getMessage(), Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+                dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+
+    }});
+
+        dialog.show();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Register");
+        dialog.setMessage("Please use email to register");
+        LayoutInflater inflator= LayoutInflater.from(this);
+        View register_layout= inflator.inflate(R.layout.layout_register,null);
+        final MaterialEditText edtEmail = register_layout.findViewById(R.id.edtEmail);
+        final MaterialEditText edtName = register_layout.findViewById(R.id.edtName);
+        final MaterialEditText edtPassword = register_layout.findViewById(R.id.edtPassword);
+        final MaterialEditText edtPhone = register_layout.findViewById(R.id.edtPhone);
+
+        dialog.setView(register_layout);
+
+        dialog.setPositiveButton("REGISTER", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (TextUtils.isEmpty(edtEmail.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter email address", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtName.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter Name", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your contact", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(edtPassword.getText().toString())) {
+                    Snackbar.make(rootLayout, "Please enter your password", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if (edtPassword.getText().toString().length() < 6) {
+                    Snackbar.make(rootLayout, "Password is too short!!!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                auth.createUserWithEmailAndPassword(edtEmail.getText().toString(), edtPassword.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        User user = new User();
+                        user.setEmail(edtEmail.getText().toString());
+                        user.setName(edtName.getText().toString());
+                        user.setPassword(edtPassword.getText().toString());
+                        user.setPhone(edtPhone.getText().toString());
+
+                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Snackbar.make(rootLayout,"Register successfully ",Snackbar.LENGTH_SHORT ).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Snackbar.make(rootLayout,"Failed!! "+e.getMessage(),Snackbar.LENGTH_SHORT ).show();
+                            }
+                        });
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(rootLayout,"Failed!! "+e.getMessage(),Snackbar.LENGTH_SHORT ).show();
+                    }
+                });
+
+            }
+        });
+
+
+                dialog.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                dialog.show();
+            }
+        }
